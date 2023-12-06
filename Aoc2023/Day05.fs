524fea27b2947d9c30ec83ad7d5f4b6e5df52b6e
Joonas Sarajärvi <muep@iki.fi>
2023-12-06 23:39:35 +0100

    Attempt at optimizing part2

--- Aoc2023/Day05.fs
+++ Aoc2023/Day05.fs
@@ -21,6 +21,26 @@ let rangeFromLine (line: string) =
     | [| dst; src; len |] -> (System.UInt64.Parse dst), (System.UInt64.Parse src), (System.UInt64.Parse len)
     | _ -> invalidArg line "Expected three numbers"
 
+let foldRanges (ranges: (uint64 * uint64) list) (start: uint64, len: uint64) =
+    match ranges with
+    | [] -> [ (start, len) ]
+    | (prevStart, prevLen) :: olderRanges ->
+        let prevEnd = prevStart + prevLen
+
+        if start <= prevEnd then
+            let thisEnd = start + len
+            let realEnd = System.UInt64.Max(prevEnd, thisEnd)
+            (prevStart, realEnd - prevStart) :: olderRanges
+        else
+            (start, len) :: (prevStart, prevLen) :: olderRanges
+
+let mergeRanges (ranges: (uint64 * uint64) array) =
+    ranges
+    |> Array.sort
+    |> Seq.fold foldRanges []
+    |> Seq.toArray
+    |> Array.rev
+
 let part1SeedsFromLine (line: string) =
     line.Substring(6).Split()
     |> Array.filter (fun s -> s.Length > 0)
@@ -34,8 +54,10 @@ let part2SeedsFromLine (line: string) =
     |> Array.chunkBySize 2
     |> Array.map (fun chunk ->
         match chunk with
-        | [| start; len |] -> seq { for n in start .. (start + len - 1UL) -> n }
-        | _ -> Seq.empty)
+        | [| start; len |] -> start, len
+        | _ -> 0UL, 0UL)
+    |> mergeRanges
+    |> Array.map (fun (start, len) -> seq { for n in start .. (start + len - 1UL) -> n })
     |> Seq.concat
 
 let foldLine seedsFromLine state (line: string) =
@@ -103,3 +125,31 @@ let ``funFronRanges basics`` () =
 
     Assert.Equal(50UL, fn 98UL)
     Assert.Equal(51UL, fn 99UL)
+
+[<Fact>]
+let ``mergeRanges basic separate`` () =
+    let input = [|(2UL, 2UL);(5UL, 2UL)|]
+    let expected = [|(2UL, 2UL);(5UL, 2UL)|] |> Seq.toList
+    let actual = mergeRanges input |> Seq.toList
+    Assert.True(actual.Equals(expected), $"Expected {expected}, got {actual}")
+
+[<Fact>]
+let ``mergeRanges basic sans overlap`` () =
+    let input = [|(0UL, 5UL);(5UL, 5UL)|]
+    let expected = [|(0UL, 10UL)|] |> Seq.toList
+    let actual = mergeRanges input |> Seq.toList
+    Assert.True(actual.Equals(expected), $"Expected {expected}, got {actual}")
+
+[<Fact>]
+let ``mergeRanges basic with overlap`` () =
+    let input = [|(0UL, 5UL);(4UL, 5UL)|]
+    let expected = [|(0UL, 9UL)|] |> Seq.toList
+    let actual = mergeRanges input |> Seq.toList
+    Assert.True(actual.Equals(expected), $"Expected {expected}, got {actual}")
+
+[<Fact>]
+let ``mergeRanges basic with overlap - different offset`` () =
+    let input = [|(1UL, 5UL);(5UL, 5UL)|]
+    let expected = [|(1UL, 9UL)|] |> Seq.toList
+    let actual = mergeRanges input |> Seq.toList
+    Assert.True(actual.Equals(expected), $"Expected {expected}, got {actual}")
